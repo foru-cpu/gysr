c0d9f59f46baf96a26bf93245e44372e494e9e2f
foru-cpu <forfinns@gmail.com>
2025-10-15 22:05:18 +0300

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // ← this creates the static `out/` folder
+};
+
+module.exports = nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, Firebase + Next.js!</h1>
+      <p>Your single-page site is ready to deploy.</p>
+    </div>
+  );
+}
